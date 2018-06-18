a0819e34087365d772d9d5eab6de1777276175e2
Lizz Thabet <lizzthabet@gmail.com>
2018-06-17 22:16:05 -0400

    perf: remove all unnecessary dependencies and files

--- browser/index.js
+++ browser/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store'
 import App from './App'
 import './styles/main.css'
 
 ReactDOM.render(
-	<Provider store = { store }>
-		<App />
-	</Provider>,
+		<App />,
 	document.getElementById('app')
 )
--- server/server.js
+++ server/server.js
@@ -1,19 +1,9 @@
 const express = require('express')
-const morgan = require('morgan')
 const path = require('path')
-const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(morgan('dev')) // logging middleware
-
-app.use(bodyParser.json()) // json parsing middleware
-app.use(bodyParser.urlencoded({ extended: true })) // urlencoded parsing middleware
-
 app.use(express.static(path.join(__dirname, '../public'))) // serve public files
-
-// routes
-app.use('/api', require('./api')) // api router
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../public/index.html'))
