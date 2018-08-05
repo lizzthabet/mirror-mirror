94f23e85c7364569699efd805766d8ea8c892e0b
Lizz Thabet <lizzthabet@gmail.com>
2018-08-05 18:05:04 -0400

    fix(https): add check for secure connection and reroute if connection isnt secure

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const path = require('path')
-const httpsRedirect = require('express-https-redirect')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '../public'))) // serve public files
-
-app.use('/', httpsRedirect()) // redirect all unsecure connections to https
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/index.html'))
+app.get('/', (req, res) => {
+	if (!req.secure) res.redirect(301, 'https://mirror-mirror.lizzthabet.com')
+	else res.sendFile(path.join(__dirname, '../public/index.html'))
 })
+
+app.use(express.static(path.join(__dirname, '../public'))) // serve public files
 
 app.use((error, req, res, next) => {
 	console.error(error)
