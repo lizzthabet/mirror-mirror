4fee37450d9ee0b49e3f911a377666d5fd084665
lizz thabet <lizzthabet@gmail.com>
2019-04-07 15:08:09 -0400

    chore(start): remove chalk dependency on start

--- server/start.js
+++ server/start.js
@@ -1,7 +1,6 @@
 const app = require('./server')
-const chalk = require('chalk')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-	console.log(chalk.inverse(`server up and running on port ${port}`))
+	console.log(`server up and running on port ${port}`)
 })
