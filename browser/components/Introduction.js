0cfd32357d5927cd3c857384e9c2816068f8af38
lizz thabet <lizzthabet@gmail.com>
2022-11-20 11:58:29 -0500

    Update copy on modal and publish new version

--- browser/components/Introduction.js
+++ browser/components/Introduction.js
@@ -21,8 +21,8 @@ export default class Introduction extends Component {
 			<ReactModal isOpen={ this.state.showModal } contentLabel='Introduction to Mirror-Mirror' onRequestClose={ this.handleCloseModal } overlayClassName='modal' className='modal__content'>
 				<h1 className='modal__heading'>Mirror, mirror on my screen <br /> Tell me, show me, how I seem</h1>
 				<p className='modal__text'><i>Mirror, mirror</i> is a playful, interactive interface that changes depending on how you look at it. It's an experiment in the performance of looking at an interface looking back at you. Twist your head, close your eyes, smile wide, look closer.</p>
-				<p>Get started by allowing this site to access your webcam. (If you don't see a webcam prompt, <a href='https://mirror-mirror-on-my-screen.herokuapp.com/'>try a secure connection</a>.)</p>
-				<p className='modal__text'>View the codebase on <a href='https://github.com/lizzthabet/mirror-mirror'>GitHub</a>.</p>
+				<p>Get started by allowing this site to access your webcam.</p>
+				<p className='modal__text'>View the code for this project on <a href='https://github.com/lizzthabet/mirror-mirror'>GitHub</a>.</p>
 				<button type='button' onClick={ this.handleCloseModal }>Close</button>
 			</ReactModal>
 		)
